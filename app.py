a8c45cf4e54cb81a5f491e8ba45fa87d596ca6ad
boddicheg <boddicheg@gmail.com>
2024-01-08 22:21:02 +0200

    Library > Series > Volumes displaying

--- app.py
+++ app.py
@@ -3,42 +3,38 @@ from tkinter import ttk
 import tkinter.messagebox
 import customtkinter
 from PIL import Image, ImageTk
-import requests 
-# import xml.etree.ElementTree as ET
-from lxml import etree
+from kavita import * 
+from enum import Enum
+import io
 
 import feedparser
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-URL = "http://192.168.5.49:5001/api/opds"
+URL = "http://192.168.5.49:5001/api/"
 API_KEY = "8df0fde8-8229-464c-ae0c-fd58a1a35b11"
 PREFIX = "{http://www.w3.org/2005/Atom}"
-THUMB_PATH = "./thumbnail.png"
+THUMB_PATH = "./assets/thumbnail.png"
+CACHE_PATH = "./cache"
 
-class OPDS:
-    def __init__(self, url, api_key):
-        self.root_url = url + "/" + api_key
-    
-    def get_entries(self, uri):
-        print(f"\n->> GET {self.root_url}{uri}\n")
-        parsed_feed = feedparser.parse(self.root_url + uri)
+WIDTH = 1280
+HEIGHT = 720
 
-        entries = []
-        for i, entry in enumerate(parsed_feed.entries, start=1):
-            entries.append({
-                "title": entry.title,
-                "link": entry.links[0].href,
-                "id": entry.id
-            })
-            # print(f"\nEntry {i}:")
-            print(entry)
-        return entries
+username = "boddicheg"
+password = "dyd6ZNU.aby*mqd6fwd"
+api_key = "8df0fde8-8229-464c-ae0c-fd58a1a35b11"
+
+class EntryType(Enum):
+    SHELF = 1
+    LIBRARY = 2
+    SERIE = 3
+    VOLUME = 4
+    PICTURE = 5
     
 class CTkLabelEx(customtkinter.CTkLabel):
-    def __init__(self, master, text, text_color, image):
-        customtkinter.CTkLabel.__init__(self, master=master, text=text, text_color=text_color, image=image)
+    def __init__(self, master, text, text_color, image = None, fg_color="transparent"):
+        customtkinter.CTkLabel.__init__(self, master=master, text=text, text_color=text_color, image=image, fg_color=fg_color)
         self.metadata = {}
     
     def set_metadata(self, md):
@@ -52,16 +48,31 @@ class App(customtkinter.CTk):
         super().__init__()
         
         self.title("Manga4Deck")
-        self.geometry(f"{1280}x{720}")
+        self.geometry(f"{WIDTH}x{HEIGHT}")
         
-        self.opds = OPDS(URL, API_KEY)
-        root_entries = self.opds.get_entries('/')
+        self.kavita = KavitaAPI(URL, username, password, api_key)
+        self.history = [{ "type": EntryType.SHELF, "parent_id": -1}]
+        self.draw()
 
-        self.draw_tites(root_entries)
-        self.history = ["/"]
+        self.bind("<BackSpace>", self.back_in_history)
+        
+    def draw(self):
+        entries = []
+        state = self.history[-1]["type"]
+        parent = self.history[-1]["parent_id"]
+        if state == EntryType.SHELF:
+            entries.append({
+                "id": -1,
+                "title": "Kavita"
+            })
+        elif state == EntryType.LIBRARY:
+            entries = self.kavita.get_library()
+        elif state == EntryType.SERIE:
+            entries = self.kavita.get_series(parent)
+        elif state == EntryType.VOLUME:
+            entries = self.kavita.get_volumes(parent)
             
-    def draw_tites(self, tiles):
-        for i, entry in enumerate(tiles, start=0):
+        for i, entry in enumerate(entries, start=0):
             col = int(i % 8)
             row = int(i / 8)
             
@@ -69,40 +80,47 @@ class App(customtkinter.CTk):
             tile.grid(row=row, column=col, padx=5, pady=5)
             
             # add image and title
-            img = ImageTk.PhotoImage(Image.open(THUMB_PATH).resize((150, 200)))
-            label = CTkLabelEx(tile, text=entry["title"], text_color='white', image=img)
+            if state == EntryType.SERIE:
+                filepath = self.kavita.get_serie_cover(entry["id"])
+                img = ImageTk.PhotoImage(Image.open(filepath).resize((150, 200))) 
+            elif state == EntryType.VOLUME:
+                filepath = self.kavita.get_volume_cover(entry["id"])
+                img = ImageTk.PhotoImage(Image.open(filepath).resize((150, 200))) 
+            else:
+                img = ImageTk.PhotoImage(Image.open(THUMB_PATH).resize((150, 200)))
+            title = entry["title"][0:14] + "..." if len(entry["title"]) > 13 else entry["title"]
+            label = CTkLabelEx(tile, text=title, text_color='white', fg_color="black" )
+            label.configure(image=img)# = img
             label.set_metadata(entry)
             label.place(relx=.5, rely=.5, anchor='center')
             label.bind("<Double-1>", self.OnDoubleClick)
             label.pack()
             
-            self.bind("<BackSpace>", self.back_in_history)
-
     def clean_master(self):
         for child in self.winfo_children():
             child.destroy()
         
     def back_in_history(self, event):
-        print("->> BACK <<-")
         if len(self.history) > 1:
             self.history.pop()
-            last_url = self.history[-1]
             self.clean_master()
-            sub_entries = self.opds.get_entries(last_url)
-            self.draw_tites(sub_entries)
+            self.draw()
         
     def OnDoubleClick(self, event):
-        print(event.widget.master.get_metadata())
-        item_metadata = event.widget.master.get_metadata()
-
-        if "link" in item_metadata.keys():
-            last_uri_section = item_metadata["link"].rsplit('/', 1)[-1]
-            last_in_history = self.history[-1]
-            full_url = last_in_history + last_uri_section + "/"
-            sub_entries = self.opds.get_entries(full_url)
-            self.history.append(full_url)
-            self.clean_master()
-            self.draw_tites(sub_entries)
+        metadata = event.widget.master.get_metadata()
+        print(metadata)
+        
+        last_in_history = self.history[-1]["type"]
+        
+        if last_in_history == EntryType.SHELF:
+            self.history.append({ "type": EntryType.LIBRARY, "parent_id": -1})
+        elif last_in_history == EntryType.LIBRARY:
+            self.history.append({ "type": EntryType.SERIE, "parent_id": metadata["id"]})
+        elif last_in_history == EntryType.SERIE:
+            self.history.append({ "type": EntryType.VOLUME, "parent_id": metadata["id"]})
+        
+        self.clean_master()
+        self.draw()
             
         print("history no is ", self.history)
         
