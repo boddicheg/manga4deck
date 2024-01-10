7835d6cf86d69368ea62a55d074cef3c9d3018ca
boddicheg <boddicheg@gmail.com>
2024-01-11 00:13:50 +0200

    Caching covers

--- kavita.py
+++ kavita.py
@@ -4,6 +4,7 @@ from icecream import ic
 import time 
 import os
 import hashlib
+import atexit
 
 CACHE_FOLDER = "./cache"
 
@@ -11,6 +12,21 @@ class KavitaAPI():
     def __init__(self, url, username, password, api_key):
         self.url = url
         self.api_key = api_key
+        
+        atexit.register(self.destuctor)
+
+        self.cache_thumbnail = []
+        self.cache_thumbnail_file = CACHE_FOLDER + "/cache_thumbnail.json"
+        # self.cache_manga = []
+        # self.cache_manga_file = CACHE_FOLDER + "/cache_manga.json"
+        
+        if not os.path.exists(self.cache_thumbnail_file):
+            with open(self.cache_thumbnail_file, 'w') as f:
+                f.write(json.dumps(self.cache_thumbnail, indent=4))
+        else:
+            with open(self.cache_thumbnail_file, 'r') as f:
+                self.cache_thumbnail = json.load(f)
+
         response = requests.post(
             self.url + "Account/login", 
             json={
@@ -19,17 +35,6 @@ class KavitaAPI():
                 "apiKey": api_key
             }
         )
-        
-        if not os.path.exists("./cache"):
-            os.mkdir("./cache")
-        else:
-            for filename in os.listdir(CACHE_FOLDER):
-                file_path = os.path.join(CACHE_FOLDER, filename)
-                try:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.unlink(file_path)
-                except Exception as e:
-                    print('[!] Failed to delete %s. Reason: %s' % (file_path, e))
         
         if len(response.content.decode()) == 0:
             raise("[!] Authentification failed!")
@@ -41,6 +46,22 @@ class KavitaAPI():
             print(f"Logged as {self.logged_as}")
         else:
             raise("[!] Authentification failed!")
+        
+    def destuctor(self):
+        with open(self.cache_thumbnail_file, 'w') as f:
+            f.write(json.dumps(self.cache_thumbnail, indent=4))
+        
+    def search_in_cache(self, key, value):
+        for e in self.cache_thumbnail:
+            if key in e.keys() and e[key] == value:
+                return e["file"]
+        return ""
+    
+    def store_in_cache(self, key, value, filename):
+        self.cache_thumbnail.append({
+            key: value,
+            "file": filename
+        })
 
     def get_library(self):
         response = requests.get(
@@ -90,6 +111,10 @@ class KavitaAPI():
         return result
     
     def get_serie_cover(self, serie):
+        filename = self.search_in_cache("seriesId", serie)
+        if len(filename) > 0:
+            return filename
+        
         url = self.url + f"image/series-cover?seriesId={serie}&apiKey={self.api_key}"
         # print(f"url: {url}")
         response = requests.get(
@@ -103,12 +128,17 @@ class KavitaAPI():
         
         with open(filename, 'wb') as f:
             f.write(response.content)
-        
+            
+        self.store_in_cache("seriesId", serie, filename)
         return filename
     
-    def get_volume_cover(self, serie):
-        url = self.url + f"image/volume-cover?volumeId={serie}&apiKey={self.api_key}"
-        # print(f"url: {url}")
+    def get_volume_cover(self, volume):
+        filename = self.search_in_cache("volumeId", volume)
+        if len(filename) > 0:
+            return filename
+
+        url = self.url + f"image/volume-cover?volumeId={volume}&apiKey={self.api_key}"
+        print(f"url: {url}")
         response = requests.get(
             url,
             headers={
@@ -120,7 +150,8 @@ class KavitaAPI():
         
         with open(filename, 'wb') as f:
             f.write(response.content)
-        
+            
+        self.store_in_cache("volumeId", volume, filename)
         return filename
         
     def get_volumes(self, parent):
@@ -135,14 +166,52 @@ class KavitaAPI():
         result = []
         if len(response.content.decode()) > 0:
             data = json.loads(response.content)
+            # ic(data)
             for vol in data["volumes"]:
-                result.append({
-                    "id": vol["id"],
-                    "title": vol["name"] + f"({vol['pagesRead']}/{vol['pages']})",
-                    "read": vol['pagesRead'],
-                    "pages": vol['pages']
-                })
+                if len(vol["chapters"]) > 0:
+                    chapter_id = vol["chapters"][0]['id']
+                    result.append({
+                        "id": vol['id'],
+                        "chapter_id": chapter_id,
+                        "title": vol["name"] + f"({vol['pagesRead']}/{vol['pages']})",
+                        "read": vol['pagesRead'],
+                        "pages": vol['pages']
+                    })
         
         return result
-            
+    
+    def get_picture(self, id, page):
+        # http://192.168.5.49:5001/api/reader/image?chapterId=1498&apiKey=8df0fde8-8229-464c-ae0c-fd58a1a35b11&page=3
+        url = self.url + f"reader/image?chapterId={id}&apiKey={self.api_key}&page={page}"
+        print(f"url: {url}")
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "image/png",
+            }
+        )
+        filename = CACHE_FOLDER + "/" + hashlib.md5(str(time.time()).encode()).hexdigest() + ".png"
+        
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        
+        return filename
+    
+    def save_progress(self, ids):
+        url = self.url + f"reader/progress"
+        requests.post(
+            url,
+            json = {
+                "libraryId": ids["libraryId"],
+                "seriesId": ids["seriesId"],
+                "volumeId": ids["volumeId"],
+                "chapterId": ids["chapterId"],
+                "pageNum": ids["pageNum"],
+            } ,
+            headers={
+                "Accept": "application/json",
+                "Authorization": f"Bearer {self.token}"
+            }
+        )
+        
         
