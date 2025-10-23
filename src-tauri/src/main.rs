34a765bc4809e40afdb8f79b0c4c52b42c0b8a4c
boddicheg <bbobyr@logitech.com>
2025-10-23 16:26:27 +0300

    Bump to 5.0.5

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -273,9 +273,6 @@ async fn start_server() {
 }
 
 fn main() {
-    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1"); 
-    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
-
     tauri::Builder::default()
         .setup(|_| {
             std::thread::spawn(|| {
